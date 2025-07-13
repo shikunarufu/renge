8e3d64920a2e9a31f09e181f091c7b2b1063a425
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-07-13 23:21:30 +0900

    Update 1-install.sh

--- 1-install.sh
+++ 1-install.sh
@@ -9,6 +9,16 @@ consolefont="Lat2-Terminus16"
 ssd1="sdb"
 hdd1="sda"
 
+timezone="Asia/Manila"
+utflocale="en_GB.UTF-8 UTF-8"
+isolocale="en_GB ISO-8859-1"
+lang="en_GB.UTF-8"
+
+hostname="Renge"
+rpasswd="nyanpasu"
+username="Shiku"
+upasswd="narufu"
+
 
 # Colors
 color_off="\033[0m"
@@ -17,45 +27,54 @@ light_gray="\033[0;37m"
 white="\033[1;37m"
 
 
+# Prefix
+prefix1="         "
+prefix2=""${light_gray}"[  "${green}"OK  "${light_gray}"] "
+
+
 # Clear the terminal screen
-echo -e "         "${light_gray}"Clearing "${white}"Terminal Screen"${color_off}""
+echo -e ""${prefix1}""${light_gray}"Clearing "${white}"Terminal Screen"${color_off}""
 clear
-echo -e ""${light_gray}"[  "${green}"OK  "${light_gray}"] Cleared "${white}"Terminal Screen"${color_off}""
+echo -e ""${prefix2}""${light_gray}"Cleared "${white}"Terminal Screen"${color_off}""
 
 
 # Set the console keyboard layout and font
-echo -e "         "${light_gray}"Setting Console Keyboard Layout"
+echo -e ""${prefix1}""${light_gray}"Setting "${white}"Console Keyboard Layout"${color_off}""
 loadkeys "${consolekeyboard}"
-echo -e ""${light_gray}"[  "${green}"OK  "${light_gray}"] Set "${white}"Console Keyboard Layout"${color_off}""
-echo -e ""${green}"Setting the console font"
+echo -e ""${prefix2}""${light_gray}"Set "${white}"Console Keyboard Layout"${color_off}""
+echo -e ""${prefix1}""${light_gray}"Setting "${white}"Console Font"${color_off}""
 setfont "${consolefont}"
+echo -e ""${prefix2}""${light_gray}"Set "${white}"Console Font"${color_off}""
 
 
 # Verify the boot mode
-echo -e ""${green}"Verifying the boot mode"
+echo -e ""${prefix1}""${light_gray}"Verifying "${white}"Boot Mode"${color_off}""
 bootmode=$(< /sys/firmware/efi/fw_platform_size)
 if [[ "${bootmode}" == "64" ]]; then
-  echo -e ""${green}"System is booted in UEFI mode and has a 64-bit x64 UEFI"
+  echo -e ""${prefix1}""${light_gray}"System is booted in UEFI mode and has a 64-bit x64 UEFI"${color_off}""
 elif [[ "${bootmode}" == "32" ]]; then
-  echo -e ""${green}"System is booted in UEFI mode and has a 32-bit IA32 UEFI"
+  echo -e ""${prefix1}""${light_gray}"System is booted in UEFI mode and has a 32-bit IA32 UEFI"${color_off}""
 else
-  echo -e ""${green}"System may be booted in BIOS (or CSM) mode"
-  echo -e ""${green}"Refer to your motherboard's manual"
+  echo -e ""${prefix1}""${light_gray}"System may be booted in BIOS (or CSM) mode"${color_off}""
+  echo -e ""${prefix1}""${light_gray}"Refer to your motherboard's manual"${color_off}""
 fi
+echo -e ""${prefix2}""${light_gray}"Verified "${white}"Boot Mode"${color_off}""
 
 
 # Connect to the internet
-echo -e ""${green}"Connecting to the internet"
+echo -e ""${prefix1}""${light_gray}"Connecting To "${white}"Internet"${color_off}""
 ping -c 5 archlinux.org
+echo -e ""${prefix2}""${light_gray}"Connected To "${white}"Internet"${color_off}""
 
 
 # Update the system clock
-echo -e ""${green}"Updating the system clock"
+echo -e ""${prefix1}""${light_gray}"Updating "${white}"System Clock"${color_off}""
 timedatectl set-ntp true
+echo -e ""${prefix2}""${light_gray}"Updated "${white}"System Clock"${color_off}""
 
 
 # Partition the disks
-echo -e ""${green}"Partitioning the disks"
+echo -e ""${prefix1}""${light_gray}"Partitioning "${white}"Disks"${color_off}""
 sgdisk --zap-all /dev/"${ssd1}"
 sgdisk --zap-all /dev/"${hdd1}"
 sgdisk --new=1:0:+1G --new=2:0:+4G --new=3:0:0 /dev/"${ssd1}"
@@ -64,17 +83,20 @@ sgdisk --typecode=1:EF00 --typecode=2:8200 --typecode=3:8300 /dev/"${ssd1}"
 sgdisk --typecode=1:8300  /dev/"${hdd1}"
 sgdisk --change-name=1:"EFI system partition" --change-name=2:"Linux swap" --change-name=3:"Linux filesystem" /dev/"${ssd1}"
 sgdisk --change-name=1:"Linux filesystem" /dev/"${hdd1}"
+echo -e ""${prefix2}""${light_gray}"Partitioned "${white}"Disks"${color_off}""
 
 
 # Reread the partition table
+echo -e ""${prefix1}""${light_gray}"Rereading "${white}"Partition Table"${color_off}""
 sleep 2
-partprobe /dev/"${ssd1}"
-partprobe /dev/"${hdd1}"
+partprobe -s /dev/"${ssd1}"
+partprobe -s /dev/"${hdd1}"
 sleep 2
+echo -e ""${prefix2}""${light_gray}"Reread "${white}"Partition Table"${color_off}""
 
 
 # Format the partitions
-echo -e ""${green}"Formatting the partitions"
+echo -e ""${prefix1}""${light_gray}"Formatting "${white}"Partitions"${color_off}""
 root_partition="${ssd1}3"
 home_partition="${hdd1}1"
 swap_partition="${ssd1}2"
@@ -83,40 +105,85 @@ mkfs.ext4 /dev/"${root_partition}"
 mkfs.ext4 /dev/"${home_partition}"
 mkswap /dev/"${swap_partition}"
 mkfs.fat -F 32 /dev/"${efi_system_partition}"
+echo -e ""${prefix2}""${light_gray}"Formatted "${white}"Partitions"${color_off}""
 
 
 # Mount the file systems
-echo -e ""${green}"Mounting the file systems"
+echo -e ""${prefix1}""${light_gray}"Mounting "${white}"File Systems"${color_off}""
 mount /dev/"${root_partition}" /mnt
 mkdir /mnt/boot
 mount /dev/"${efi_system_partition}" /mnt/boot
 mkdir /mnt/home
 mount /dev/"${home_partition}" /mnt/home
 swapon /dev/"${swap_partition}"
+echo -e ""${prefix2}""${light_gray}"Mounted "${white}"File Systems"${color_off}""
 
 
 # Select the mirrors
-echo -e ""${green}"Selecting the mirrors"
-reflector --save /etc/pacman.d/mirrorlist --sort rate --verbose -f 20 -l 200 -p https,http
+echo -e ""${prefix1}""${light_gray}"Selecting "${white}"Mirrors"${color_off}""
+#reflector --save /etc/pacman.d/mirrorlist --sort rate --verbose -f 20 -l 200 -p https,http
+echo -e ""${prefix2}""${light_gray}"Selected "${white}"Mirrors"${color_off}""
 
 
 # Install essential packages
-echo -e ""${green}"Installing essential packages"
+echo -e ""${prefix1}""${light_gray}"Installing "${white}"Essential Packages"${color_off}""
 pacstrap -K /mnt base base-devel linux linux-firmware linux-zen linux-zen-headers amd-ucode exfatprogs ntfs-3g networkmanager neovim man-db man-pages texinfo
+echo -e ""${prefix2}""${light_gray}"Installed "${white}"Essential Packages"${color_off}""
 
 
 # Fstab
-echo -e ""${green}"Generating an fstab file"
+echo -e ""${prefix1}""${light_gray}"Generating "${white}"Fstab File"${color_off}""
 genfstab -U /mnt >> /mnt/etc/fstab
-
-
-# Scripts
-mkdir /mnt/ALIS
-cp 2-chroot.sh /mnt/ALIS
+echo -e ""${prefix2}""${light_gray}"Generated "${white}"Fstab File"${color_off}""
 
 
 # Chroot
 echo -e ""${green}"Changing root into the new system"
 arch-chroot /mnt
-chmod +x ./ALIS/2-chroot.sh
-./ALIS/2-chroot.sh
+
+
+
+
+# Time
+echo -e ""${green}"Setting the time zone"
+ln -sf /usr/share/zoneinfo/"${timezone}" /etc/localtime
+hwclock --systohc
+
+
+# Localization
+echo -e ""${green}"Generating the locales"
+sed --in-place 's/"#${utflocale}"/"${utflocale}"/' /etc/locale.gen
+sed --in-place 's/"#${isolocale}"/"${isolocale}"/' /etc/locale.gen
+locale-gen
+echo -e ""${green}"Setting the system locale"
+echo "LANG="${lang}"" >> /etc/locale.conf
+echo -e ""${green}"Setting the console keyboard layout"
+echo "KEYMAP="${consolekeyboard}"" >> /etc/vconsole.conf
+
+
+# Network configuration
+echo -e ""${green}"Configuring the network connection"
+echo "${hostname}" >> /etc/hostname
+systemctl enable NetworkManager.service
+
+
+# Root password
+echo -e ""${green}"Setting the root password"
+passwd root
+
+
+# Boot loader
+echo -e ""${green}"Installing boot loader"
+pacman -S --noconfirm grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+# User management
+echo -e ""${green}"Adding a new user"
+useradd --create-home --groups wheel "${username}"
+passwd "${username}"
+
+
+# Done
+echo -e ""${green}"Done!"
