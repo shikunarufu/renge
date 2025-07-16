c50789467721fb7d8fb667177d483a562a76225c
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-07-16 12:44:02 +0900

    Update 1-install.sh

--- 1-install.sh
+++ 1-install.sh
@@ -2,37 +2,39 @@
 #
 # Shiku's Arch Linux Installation Script
 
+#######################################
+# Pre-Installation
+#######################################
+
 # Configuration
 consolekeyboard="us"
 consolefont="Lat2-Terminus16"
 ssd1="sdb"
 hdd1="sda"
-
-#timezone="Asia/Manila"
-#utflocale="en_GB.UTF-8 UTF-8"
-#isolocale="en_GB ISO-8859-1"
-#lang="en_GB.UTF-8"
-
-#hostname="Renge"
-#rpasswd="nyanpasu"
+timezone="Asia/Manila"
+utflocale="en_GB.UTF-8 UTF-8"
+isolocale="en_GB ISO-8859-1"
+lang="en_GB.UTF-8"
+hostname="Renge"
+rpasswd="nyanpasu"
 #username="Shiku"
 #upasswd="narufu"
 
-
-# Aesthetic
+# Aesthetics
 entry_status() {
   printf "\e[10G"
   if [[ $1 == *" "* ]]; then
     local subject=${1%% *}
     local predicate=${1#* }
-    printf "%s \e[1;37m%s\033[0m\n" "${subject}" "${predicate}"
-    
+    printf "%s \e[1;37m%s\e[0m\n" "${subject}" "${predicate}"
   else
-    printf "%s" "$1"
+    printf "%s\n" "$1"
   fi
 }
 info_status() {
   printf "\e[10G"
+  local text="$1"
+  printf "%s\n" "$1"
 }
 exit_status() {
   printf "["
@@ -40,15 +42,19 @@ exit_status() {
   printf "  OK  "
   printf "\e[0m"
   printf "]"
-  entry_status
+  if [[ $1 == *" "* ]]; then
+    local subject=${1%% *}
+    local predicate=${1#* }
+    printf "%s \e[1;37m%s\e[0m\n" "${subject}" "${predicate}"
+  else
+    printf "%s\n" "$1"
+  fi
 }
-
 
 # Clear the terminal screen
 entry_status "Clearing Terminal Screen"
 clear
 exit_status "Cleared Terminal Screen"
-
 
 # Set the console keyboard layout and font
 entry_status "Setting Console Keyboard Layout"
@@ -58,79 +64,74 @@ entry_status "Setting Console Font"
 setfont "${consolefont}"
 exit_status "Set Console Font to ${consolefont}"
 
-
 # Verify the boot mode
 entry_status "Verifying Boot Mode"
 bootmode=$(< /sys/firmware/efi/fw_platform_size)
 if [[ "${bootmode}" == "64" ]]; then
-  entry_status "System is booted in UEFI mode and has a 64-bit x64 UEFI"
+  info_status "System is booted in UEFI mode and has a 64-bit x64 UEFI"
 elif [[ "${bootmode}" == "32" ]]; then
-  entry_status "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
+  info_status "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
 else
-  entry_status "System may be booted in BIOS (or CSM) mode"
-  entry_status "Refer to your motherboard's manual"
+  info_status "System may be booted in BIOS (or CSM) mode"
+  info_status "Refer to your motherboard's manual"
 fi
 exit_status "Verified Boot Mode"
-
 
 # Connect to the internet
 entry_status "Connecting to Internet"
 ping -c 5 archlinux.org > /dev/null 2>&1
 exit_status "Connected to Internet"
 
-
 # Update the system clock
 entry_status "Updating System Clock"
 timedatectl set-ntp true
 exit_status "Updated System Clock"
 
-
 # Partition the disks
 entry_status "Partitioning Disks"
 entry_status "Destroying GPT and MBR Data Structures"
-#sgdisk --zap-all /dev/"${ssd1}" > /dev/null 2>&1
+sgdisk --zap-all /dev/"${ssd1}" > /dev/null 2>&1
 exit_status "Destroyed GPT and MBR Data Structures in /dev/${ssd1}"
 entry_status "Destroying GPT and MBR Data Structures"
-#sgdisk --zap-all /dev/"${hdd1}" > /dev/null 2>&1
+sgdisk --zap-all /dev/"${hdd1}" > /dev/null 2>&1
 exit_status "Destroyed GPT and MBR Data Structures in /dev/${hdd1}"
 entry_status "Erasing All Available Signatures"
-#wipefs --all --force /dev/"${ssd1}"
+wipefs --all --force /dev/"${ssd1}"
 exit_status "Erased All Available Signatures in /dev/${ssd1}"
 entry_status "Erasing All Available Signatures"
-#wipefs --all --force /dev/"${hdd1}"
+wipefs --all --force /dev/"${hdd1}"
 exit_status "Erased All Available Signatures in /dev/${hdd1}"
 entry_status "Rereading Partition Table"
-#partprobe /dev/"${ssd1}"
+partprobe /dev/"${ssd1}"
 exit_status "Reread Partition Table in /dev/${ssd1}"
 entry_status "Rereading Partition Table"
-#partprobe /dev/"${hdd1}"
+partprobe /dev/"${hdd1}"
 exit_status "Reread Partition Table in /dev/${hdd1}"
 entry_status "Creating New Partition"
-#sgdisk --new=1:0:+1G --new=2:0:+4G --new=3:0:0 /dev/"${ssd1}" > /dev/null 2>&1
+sgdisk --new=1:0:+1G --new=2:0:+4G --new=3:0:0 /dev/"${ssd1}" > /dev/null 2>&1
 exit_status "Created New Partition in /dev/${ssd1}"
 entry_status "Creating New Partition"
-#sgdisk --new=1:0:0 /dev/"${hdd1}" > /dev/null 2>&1
+sgdisk --new=1:0:0 /dev/"${hdd1}" > /dev/null 2>&1
 exit_status "Created New Partition in /dev/${hdd1}"
 entry_status "Changing Partition Type Code"
-#sgdisk --typecode=1:EF00 --typecode=2:8200 --typecode=3:8300 /dev/"${ssd1}" > /dev/null 2>&1
+sgdisk --typecode=1:EF00 --typecode=2:8200 --typecode=3:8300 /dev/"${ssd1}" > /dev/null 2>&1
 exit_status "Changed Partition Type Code in /dev/${ssd1}"
 entry_status "Changing Partition Type Code"
-#sgdisk --typecode=1:8300  /dev/"${hdd1}" > /dev/null 2>&1
+sgdisk --typecode=1:8300  /dev/"${hdd1}" > /dev/null 2>&1
 exit_status "Changed Partition Type Code in /dev/${hdd1}"
 entry_status "Changing GPT Name of Partition"
-#sgdisk --change-name=1:"EFI system partition" --change-name=2:"Linux swap" --change-name=3:"Linux filesystem" /dev/"${ssd1}" > /dev/null 2>&1
+sgdisk --change-name=1:"EFI system partition" --change-name=2:"Linux swap" --change-name=3:"Linux filesystem" /dev/"${ssd1}" > /dev/null 2>&1
 exit_status "Changed GPT Name of Partition in /dev/${ssd1}"
 entry_status "Changing GPT Name of Partition"
-#sgdisk --change-name=1:"Linux filesystem" /dev/"${hdd1}" > /dev/null 2>&1
+sgdisk --change-name=1:"Linux filesystem" /dev/"${hdd1}" > /dev/null 2>&1
 exit_status "Changed GPT Name of Partition in /dev/${hdd1}"
 entry_status "Rereading Partition Table"
-#partprobe /dev/"${ssd1}"
+partprobe /dev/"${ssd1}"
 exit_status "Reread Partition Table in /dev/${ssd1}"
 entry_status "Rereading Partition Table"
-#partprobe /dev/"${hdd1}"
+partprobe /dev/"${hdd1}"
 exit_status "Reread Partition Table in /dev/${hdd1}"
 exit_status "Partitioned Disks"
-
 
 # Format the partitions
 entry_status "Formatting Partitions"
@@ -139,63 +140,109 @@ home_partition="${hdd1}1"
 swap_partition="${ssd1}2"
 efi_system_partition="${ssd1}1"
 entry_status "Creating Ext4 File System"
-#mkfs.ext4 -F /dev/"${root_partition}"
+mkfs.ext4 -F /dev/"${root_partition}"
 exit_status "Created Ext4 File System in /dev/${root_partition}"
 entry_status "Creating Ext4 File System"
-#mkfs.ext4 -F /dev/"${home_partition}"
+mkfs.ext4 -F /dev/"${home_partition}"
 exit_status "Created Ext4 File System in /dev/${home_partition}"
 entry_status "Initializing Linux Swap"
-#mkswap /dev/"${swap_partition}"
+mkswap /dev/"${swap_partition}"
 exit_status "Initialized Linux Swap in /dev/${swap_partition}"
 entry_status "Formatting EFI System Partition"
-#mkfs.fat -F 32 /dev/"${efi_system_partition}"
+mkfs.fat -F 32 /dev/"${efi_system_partition}"
 exit_status "Formatted EFI System Partition in /dev/${efi_system_partition}"
 exit_status "Formatted Partitions"
-
 
 # Mount the file systems
 entry_status "Mounting File Systems"
 entry_status "Mounting Root Volume"
-#mount /dev/"${root_partition}" /mnt
+mount /dev/"${root_partition}" /mnt
 exit_status "Mounted Root Volume in /mnt"
 entry_status "Creating Directory for EFI System Partition"
-#mkdir /mnt/boot
+mkdir /mnt/boot
 exit_status "Created Directory for EFI System Partition in /mnt/boot"
 entry_status "Mounting EFI System Partition"
-#mount /dev/"${efi_system_partition}" /mnt/boot
+mount /dev/"${efi_system_partition}" /mnt/boot
 exit_status "Mounted EFI System Partition in /mnt/boot"
 entry_status "Creating Directory for Home Partition"
-#mkdir /mnt/home
+mkdir /mnt/home
 exit_status "Created Directory for Home Partition in /mnt/boot"
 entry_status "Mounting Home Partition"
-#mount /dev/"${home_partition}" /mnt/home
+mount /dev/"${home_partition}" /mnt/home
 exit_status "Mounted Home Partition in /mnt/home"
 entry_status "Enabling Swap Partition"
-#swapon /dev/"${swap_partition}"
+swapon /dev/"${swap_partition}"
 exit_status "Enabling Swap Partition"
 exit_status "Mounted File Systems"
 
+#######################################
+# Installation
+#######################################
 
 # Select the mirrors
 entry_status "Selecting Mirrors"
-#reflector --save /etc/pacman.d/mirrorlist --sort rate --verbose -f 20 -l 200 -p https,http
+reflector --save /etc/pacman.d/mirrorlist --sort rate --verbose -f 20 -l 200 -p https,http
 exit_status "Selected Mirrors"
-
 
 # Install essential packages
 entry_status "Installing Essential Packages"
-#pacstrap -K /mnt base base-devel linux linux-firmware linux-zen linux-zen-headers amd-ucode exfatprogs ntfs-3g networkmanager neovim man-db man-pages texinfo  > /dev/null 2>&1
+pacstrap -K /mnt base base-devel linux linux-firmware linux-zen linux-zen-headers amd-ucode exfatprogs ntfs-3g networkmanager neovim man-db man-pages texinfo > /dev/null 2>&1
 exit_status "Installed Essential Packages"
 
+#######################################
+# Configure The System
+#######################################
 
 # Fstab
 entry_status "Generating Fstab File"
-#genfstab -U /mnt >> /mnt/etc/fstab
-exit_status "Generated Fstab File /mnt/etc/fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+exit_status "Generated Fstab File in /mnt/etc/fstab"
 
+# Time
+entry_status "Setting Time Zone"
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/"${timezone}" /etc/localtime
+exit_status "Set Time Zone to ${timezone}"
+entry_status "Generating /etc/adjtime"
+arch-chroot /mnt hwclock --systohc
+exit_status "Generated /etc/adjtime"
 
-# Chroot
-entry_status "Changing root into the new system"
-#arch-chroot /mnt /bin/bash <<END
+# Localization
+entry_status "Uncommenting ${utflocale}"
+arch-chroot /mnt sed --in-place 's/"#${utflocale}"/"${utflocale}"/' /etc/locale.gen
+exit_status "Uncommented ${utflocale} in /etc/locale.gen"
+entry_status "Uncommenting ${isolocale}"
+arch-chroot /mnt sed --in-place 's/"#${isolocale}"/"${isolocale}"/' /etc/locale.gen
+exit_status "Uncommented ${isolocale} in /etc/locale.gen"
+entry_status "Generating Locales"
+arch-chroot /mnt locale-gen
+exit_status "Generated Locales"
+entry_status "Setting System Locale"
+arch-chroot /mnt echo "LANG="${lang}"" >> /etc/locale.conf
+exit_status "Set System Locale in /etc/locale.conf"
+entry_status "Setting Console Keyboard Layout"
+arch-chroot /mnt echo "KEYMAP="${consolekeyboard}"" >> /etc/vconsole.conf
+exit_status "Set Console Keyboard Layout to ${consolekeyboard}"
 
-END
+# Network configuration
+entry_status "Creating Hostname File"
+arch-chroot /mnt echo "${hostname}" >> /etc/hostname
+exit_status "Created Hostname (${hostname}) File in /etc/hostname"
+entry_status "Enabling NetworkManager.service"
+arch-chroot /mnt systemctl enable NetworkManager.service
+exit_status "Enabled NetworkManager.service"
+
+# Root password
+entry_status "Setting Root Password"
+printf "%s\n%s" "${rpasswd}" "${rpasswd}" | arch-chroot /mnt passwd
+exit_status "Set Root Password"
+
+# Boot loader
+entry_status "Installing Boot Loader"
+arch-chroot /mnt pacman -S --noconfirm grub efibootmgr
+exit_status "Installed Boot Loader (GRUB)"
+entry_status "Installing GRUB"
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+exit_status "Installed GRUB to /boot"
+entry_status "Generating Main Configuration File"
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+entry_status "Generated Main Configuration File in /boot/grub/grub.cfg"
