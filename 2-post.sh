e5b9c8b1cad1a7f965b92f9fa0315325d7e6b137
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-07-18 15:20:32 +0100

    Update 2-post.sh

--- 2-post.sh
+++ 2-post.sh
@@ -62,7 +62,7 @@ clear
 
 # Yay
 #entry_status "Installing Yay"
-sudo pacman -S --noconfirm --needed git base-devel
+printf "%s\n%s" "${user_passwd}" | sudo pacman -S --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
