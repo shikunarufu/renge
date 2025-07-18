557af424b041a41064185e3dcc3bb4a19fe316a0
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-07-18 12:46:07 +0100

    Update 2-post.sh

--- 2-post.sh
+++ 2-post.sh
@@ -66,7 +66,7 @@ clear
 printf "%s\n%s" "${user_passwd}" | sudo --stdin pacman -S --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
-printf "%s\n%s" "${user_passwd}" | sudo --stdin- -user="${username}" makepkg -si --noconfirm
+printf "%s\n%s" "${user_passwd}" | sudo --stdin --user="${username}" makepkg -si --noconfirm
 #exit_status "Installed Yay"
 #entry_status "Generating Development Package Database"
 yay --yay --gendb --noconfirm
