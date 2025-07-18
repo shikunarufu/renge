3d99e7818eb229ba636ff254b1d927ded7449249
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-07-18 12:42:04 +0100

    Update 2-post.sh

--- 2-post.sh
+++ 2-post.sh
@@ -18,6 +18,7 @@ set -x
 
 # Configuration
 user_passwd="narufu"
+username="Shiku"
 
 # Aesthetics
 entry_status() {
@@ -65,7 +66,7 @@ clear
 printf "%s\n%s" "${user_passwd}" | sudo --stdin pacman -S --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
-printf "%s\n%s" "${user_passwd}" | sudo --stdin makepkg -si --noconfirm
+printf "%s\n%s" "${user_passwd}" | sudo --stdin- -user="${username}" makepkg -si --noconfirm
 #exit_status "Installed Yay"
 #entry_status "Generating Development Package Database"
 yay --yay --gendb --noconfirm
