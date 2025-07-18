62753bf7a57741e93370968aa8902b2027c2ca85
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-07-18 15:17:29 +0100

    Update 2-post.sh

--- 2-post.sh
+++ 2-post.sh
@@ -62,7 +62,7 @@ clear
 
 # Yay
 #entry_status "Installing Yay"
-pacman -S --noconfirm --needed git base-devel
+sudo pacman -S --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
