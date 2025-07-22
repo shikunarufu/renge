283d4c5f94b56578f0c4c09cd72456dc9689cb37
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-07-22 04:55:16 +0100

    Update 2-post.sh

--- 2-post.sh
+++ 2-post.sh
@@ -511,6 +511,7 @@ cat > /home/"${username}"/.config/waybar/scripts/get_weather.sh << 'EOF'
 #!/usr/bin/env bash
 for i in {1..5}; do
   text=$(curl -s "https://wttr.in/$1?format=1")
+  echo "$text"
   sleep 2
 done
 echo "{\"text\":\"error\", \"tooltip\":\"error\"}"
