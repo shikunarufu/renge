3aa42635c6ff750fdc90d6fcdb9143fc16ebc560
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-08-09 09:49:56 +0100

    Update 2-post.sh

--- 2-post.sh
+++ 2-post.sh
@@ -686,11 +686,11 @@ cat > /home/"${username}"/.config/fish/functions/fish_greeting.fish << 'EOF'
 function fish_greeting
   switch (random 1 5)
     case 1
-      echo "Shimmy Shimmy Yay, Shimmy Yay, Shimmy Ya (Drank) Swalla-la-la"
+      echo "Shimmy Shimmy Yay, Shimmy Yay, Shimmy Yaaa~ (Drank) Swalla'la'la"
     case 2
-      echo "Uh, summa-lumma, dooma-lumma, you assumin' I'm a human. What I gotta do to get it through to you? I'm superhuman. Innovative and I'm made of rubber so that anything you say is ricochetin' off of me and-"
+      echo "Uh, sama lamaa duma lamaa, you assuming I'm a human What I gotta do to get it through to you I'm superhuman Innovative and I'm made of rubber So that anything you say is ricocheting off of me and-"
     case 3
-      echo "What's this smell? Wut da heeeeeeeell, oh my god, no waaaaayy"
+      echo "What's this smell? wut da heeeeeeeell oh my god no waaaaayy"
     case 4
       echo "Super idol 的笑容都没你的甜八月正午的阳光都没你耀眼热爱105度的你滴滴清纯的蒸馏水"
     case 5
