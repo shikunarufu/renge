5526f2fdf892d28ad69e949561233223a1b4abb8
Shikunarufu <134840576+Shikunarufu@users.noreply.github.com>
2025-08-11 08:31:22 +0100

    Create fish_greeting.fish

---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,15 @@
+function fish_greeting
+  switch (random 1 5)
+    case 1
+      echo "Shimmy Shimmy Yay, Shimmy Yay, Shimmy Yaaa~ (Drank) Swalla'la'la"
+    case 2
+      echo "Uh, sama lamaa duma lamaa, you assuming I'm a human What I gotta do to get it through to you I'm superhuman Innovative and I'm made of rubber So that anything you say is ricocheting off of me and-"
+    case 3
+      echo "What's this smell? wut da heeeeeeeell oh my god no waaaaayy"
+    case 4
+      echo "Super idol 的笑容都没你的甜八月正午的阳光都没你耀眼热爱105度的你滴滴清纯的蒸馏水"
+    case 5
+      echo "近前看其牆上寫著秦香蓮年三十二歲那狀告當朝駙馬郎欺君王瞞皇上"
+  end
+  echo ""
+end
