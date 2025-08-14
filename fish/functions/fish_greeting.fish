4a30b79a5614e0a6761f286b8e3a45ec0fb6a349
shiku <134840576+shikunarufu@users.noreply.github.com>
2025-08-14 16:57:30 +0100

    Update fish_greeting.fish

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,15 +1,31 @@
 function fish_greeting
-  switch (random 1 5)
+  switch (random 1 13)
     case 1
       echo "Shimmy Shimmy Yay, Shimmy Yay, Shimmy Yaaa~ (Drank) Swalla'la'la"
     case 2
       echo "Uh, sama lamaa duma lamaa, you assuming I'm a human What I gotta do to get it through to you I'm superhuman Innovative and I'm made of rubber So that anything you say is ricocheting off of me and-"
     case 3
-      echo "What's this smell? wut da heeeeeeeell oh my god no waaaaayy"
+      echo "wut da heeeeeeeell oh my god no waaaaayy"
     case 4
       echo "Super idol 的笑容都没你的甜八月正午的阳光都没你耀眼热爱105度的你滴滴清纯的蒸馏水"
     case 5
       echo "近前看其牆上寫著秦香蓮年三十二歲那狀告當朝駙馬郎欺君王瞞皇上"
+    case 6
+      echo "雪花飘飘北风啸啸天地一片苍茫"
+    case 7
+      echo "Tylko jedno w głowie mam Koksu pięć gram odlecieć sam W krainę za zapomnienia"
+    case 8
+      echo "Kulikitakatí, kulikitakatá Kulikitakatí, kulikitakatá Sakalakatikitakatiki, sakalakatikitakatiki Sakalakatikitakatiki, sakalakatikitakatiki"
+    case 9
+      echo "Buuuryyyy the liiiiiiight deeeep withiiiiiin"
+    case 10
+      echo "I broke up with my ex girl here's her number sike! that's the wrong number!"
+    case 11
+      echo "[读白]早上好中国 bing chillin' 现在我有冰淇 bing chillin' 我很喜欢冰淇淋但是速 bing chillin' 比冰淇淋速度与激情所以... 现在是音乐时间准备.. [副歌] 两个礼拜以后《速度与激情9》两个礼拜以后《速度与激情9》两个礼拜以后《速度与激情9》[读白]不要忘记不要错过"
+    case 12
+      echo "Skibidi Bop Mm Dada"
+    case 13
+      echo "يرتوى بالحب قلبى حب خير رسلٍ ربى من به أبصرت دربى"
   end
   echo ""
 end
