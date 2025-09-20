1a37b769f6df2fa74cdf2b3e60ffd3704d8817ff
shiku <134840576+shikunarufu@users.noreply.github.com>
2025-09-20 16:06:02 +0800

    Update fish_greeting.fish

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,31 +1,70 @@
 function fish_greeting
-  switch (random 1 13)
+  fastfetch
+  switch (random 1 32)
     case 1
-      echo "Shimmy Shimmy Yay, Shimmy Yay, Shimmy Yaaa~ (Drank) Swalla'la'la"
+      printf "Sonic's Quote: Shimmy Shimmy Yay, Shimmy Yay, Shimmy Yaaa~ (Drank) Swalla'la'la"
     case 2
-      echo "Uh, sama lamaa duma lamaa, you assuming I'm a human What I gotta do to get it through to you I'm superhuman Innovative and I'm made of rubber So that anything you say is ricocheting off of me and-"
+      printf "Sonic's Quote: Uh, sama lamaa duma lamaa, you assuming I'm a human What I gotta do to get it through to you I'm superhuman Innovative and I'm made of rubber So that anything you say is ricocheting off of me and-"
     case 3
-      echo "wut da heeeeeeeell oh my god no waaaaayy"
+      printf "Sonic's Quote: wut da heeeeeeeell oh my god no waaaaayy"
     case 4
-      echo "Super idol 的笑容都没你的甜八月正午的阳光都没你耀眼热爱105度的你滴滴清纯的蒸馏水"
+      printf "Sonic's Quote: Super idol 的笑容都没你的甜八月正午的阳光都没你耀眼热爱105度的你滴滴清纯的蒸馏水"
     case 5
-      echo "近前看其牆上寫著秦香蓮年三十二歲那狀告當朝駙馬郎欺君王瞞皇上"
+      printf "Sonic's Quote: 近前看其牆上寫著秦香蓮年三十二歲那狀告當朝駙馬郎欺君王瞞皇上"
     case 6
-      echo "雪花飘飘北风啸啸天地一片苍茫"
+      printf "Sonic's Quote: 雪花飘飘北风啸啸天地一片苍茫"
     case 7
-      echo "Tylko jedno w głowie mam Koksu pięć gram odlecieć sam W krainę za zapomnienia"
+      printf "Sonic's Quote: Tylko jedno w głowie mam Koksu pięć gram odlecieć sam W krainę za zapomnienia"
     case 8
-      echo "Kulikitakatí, kulikitakatá Kulikitakatí, kulikitakatá Sakalakatikitakatiki, sakalakatikitakatiki Sakalakatikitakatiki, sakalakatikitakatiki"
+      printf "Sonic's Quote: Kulikitakatí, kulikitakatá Kulikitakatí, kulikitakatá Sakalakatikitakatiki, sakalakatikitakatiki Sakalakatikitakatiki, sakalakatikitakatiki"
     case 9
-      echo "Buuuryyyy the liiiiiiight deeeep withiiiiiin"
+      printf "Sonic's Quote: Buuuryyyy the liiiiiiight deeeep withiiiiiin"
     case 10
-      echo "I broke up with my ex girl here's her number sike! that's the wrong number!"
+      printf "Sonic's Quote: I broke up with my ex girl here's her number sike! that's the wrong number!"
     case 11
-      echo "[读白]早上好中国 bing chillin' 现在我有冰淇 bing chillin' 我很喜欢冰淇淋但是速 bing chillin' 比冰淇淋速度与激情所以... 现在是音乐时间准备.. [副歌] 两个礼拜以后《速度与激情9》两个礼拜以后《速度与激情9》两个礼拜以后《速度与激情9》[读白]不要忘记不要错过"
+      printf "Sonic's Quote: [读白]早上好中国 bing chillin' 现在我有冰淇 bing chillin' 我很喜欢冰淇淋但是速 bing chillin' 比冰淇淋速度与激情所以... 现在是音乐时间准备.. [副歌] 两个礼拜以后《速度与激情9》两个礼拜以后《速度与激情9》两个礼拜以后《速度与激情9》[读白]不要忘记不要错过"
     case 12
-      echo "Skibidi Bop Mm Dada"
+      printf "Sonic's Quote: Skibidi Bop Mm Dada"
     case 13
-      echo "يرتوى بالحب قلبى حب خير رسلٍ ربى من به أبصرت دربى"
+      printf "Sonic's Quote: يرتوى بالحب قلبى حب خير رسلٍ ربى من به أبصرت دربى"
+    case 14
+      printf "Sonic's Quote: Ring ding ding daa baa Baa aramba baa bom baa barooumba"
+    case 15
+      printf "Sonic's Quote: ਨੀਵੀਂ ਅੱਖ ਗੂੰਜ ਚੀਰ ਪਾ ਕੇ ਰੱਖ ਲੇ ਪੱਲੇ ਵਿਚ ਮੁਖੜਾ ਲੂਕਾ ਕੇ ਰੱਖ ਲੇ"
+    case 16
+      printf "Tails's Quote: America ya! Hallo! Hallo! Hallo! Hallo! Hallo!"
+    case 17
+      printf "Sonic's Quote: moonday leeeft me broken Tuesday, I was through with hoping"
+    case 18
+      printf "Sonic's Quote: 天上太阳红呀红彤彤哎心中的太阳是毛泽东啊他领导我们得解放啊人民翻身当家做主人"
+    case 19
+      printf "Sonic's Quote: I'm in the drive-thru of Burger King Can I please get a Whopper Jr. with onion rings? Make it a meal so I can get a drink No, I'm not finished, that's not everything Can I please get a double Whopper with no cheese? Can I please get a number two with a large drink? I got money, so I don't care how much it cost me So just throw in some extra fries, don't make them salty All this cheese gonna make my booty drip-drip I'm lactose intolerant, I don't sip milk If I see the sight of cheese, I'ma trip-trip I'ma sit on your toilet seat and doodoo then dip So you got my lil' Whopper Jr.? (I didn't forget that) And you got my double Whopper? (I didn't forget that) What about my onion rings? (Hold on, you can sit back) Burger King, they know me now, cheese, I don't want that And I'm getting hungry now, I know you heard that Waiting for my onion rings, so I don't have to turn back Burger King, don't play with me, y'all nuggets is so trash Nuggets taste like rabbit nipples, why y'all even serve that? Better stop playing and just give me all of my food Either I pay you right now, or leave the drive-thru Gave me the bag, and then I took a bite of my food There's cheese in my mouth im gonna doodoo I'm in the drive-thru of Burger King Man, they just gave me a Whopper Jr. with hella cheese Made it a meal, so, yes, I got my drink But why they gon' put cheese on everything They put cheese on my Double Whopper with no cheese I'll be takin' a number two in the morning Hold on, can I please be excused for a mooment? The cheese already in my body, booty farting i farted"
+    case 20
+      printf "Shadow's Quote: I'LL F*** IT"
+    case 21
+      printf "Shadow's Quote: D*CK"
+    case 22
+      printf "Shadow's Quote: GETTING C*CK"
+    case 23
+      printf "Shadow's Quote: WELL C*M"
+    case 24
+      printf "Shadow's Quote: BE C*M"
+    case 25
+      printf "Sonic's Quote: California girls, we're unforgettable Daisy Dukes, bikinis on top Sun-kissed skin so hot, we'll melt your popsicle Oh-oh-oh"
+    case 26
+      printf "Sonic's Quote: Shorty a lil' baddie She my lil' boo thing and shorty got the fatty (shorty got the fatty) Shorty be catching mood swings"
+    case 27
+      printf "Sonic's Quote: 張牙舞爪健身操！祝您健康，肉的！"
+    case 28
+      printf "Sonic's Quote: sticking out your gyat for the rizzler you're so skibidy You're so fanum tax"
+    case 29
+      printf "Sonic's Quote: Disturbing the peace look into my eyes Now tell me the things you 're blabbin' about me behind my back The tenacity i hold is hard to break down"
+    case 30
+      printf "Sonic's Quote: Gedagedigedagedago I been married along time ago Wude wude you kamfro Wude wude you go Wude wude you kamfro Buckladoo Gedagedigedagedago I been married along time ago"
+    case 31
+      printf "Sonic's Quote: Hey guy! Check out that vent Have you ever heard of Among Us? if we vent We can get to where we're going right away But be careful it will make us pretty sus"
+    case 32
+      printf "Sonic's Quote: I just lost my dawg (yeah) look at my brother taught me how to chase the bag Get right in the field Ihad to make a bag"
   end
-  echo ""
+  printf "\n\n"
 end
