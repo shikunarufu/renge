42a2b4b7a83715bd10f6e5cb6dd6b20f76bc3f75
shiku <134840576+shikunarufu@users.noreply.github.com>
2025-09-21 12:07:21 +0800

    Create vm.sh

---
+++ main/vm.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Shiku's Virtualization Installation Script
+
+# This script automates the installation process of Virtualization.
+
+# This script assumes you have already booted
+# and logged in into the new system with the user account.
+
+# This script assumes a working internet connection is available.
+
+# Uncomment the line below to show command outputs.
+# set -x
+
+#######################################
+# Preparation
+#######################################
+
+# Verify the boot mode
+# bootmode="$(cat /sys/firmware/efi/fw_platform_size)"
+# if [[ "${bootmode}" == "64" ]]; then
+#   echo "System is booted in UEFI mode and has a 64-bit x64 UEFI"
+# elif [[ "${bootmode}" == "32" ]]; then
+#   echo "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
+# else
+#   echo "System may be booted in BIOS (or CSM) mode"
+#   echo "Refer to your motherboard's manual"
+#   exit
+# fi
+
+# Verify SVM mode
+# grep --extended-regexp --only-matching 'svm' /proc/cpuinfo
+
+# Verify NX mode
+# sudo dmesg | grep 'Execute Disable'
+
+# Verify IOMMU
+# ls /sys/class/iommu/
+
+# Virtual Machine
+# sudo sed --in-place "s/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet video=efifb:off\"/g" /etc/default/grub
+# sudo grub-mkconfig -o /boot/grub/grub.cfg
+# reboot
+# yes y | sudo pacman -S virt-manager qemu-full vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf
+# sudo sed --in-place 's/#unix_sock_group = \"libvirt\"/unix_sock_group = \"libvirt\"/g' /etc/libvirt/libvirtd.conf
+# sudo sed --in-place 's/#unix_sock_rw_perms = \"0770\"/unix_sock_rw_perms = \"0770\"/g' /etc/libvirt/libvirtd.conf
+# sudo sed --in-place 's/#log_filters=\"1:qemu 1:libvirt 4:object 4:json 4:event 1:util\"/log_filters=\"3:qemu 1:libvirt\"/g' /etc/libvirt/libvirtd.conf
+# sudo sed --in-place 's/#log_outputs=\"3:syslog:libvirtd\"/log_outputs=\"2:file:/var/log/libvirt/libvirtd.log\"/g' /etc/libvirt/libvirtd.conf
+# sudo usermod --append --groups kvm,libvirt "${username}"
+# sudo systemctl enable libvirtd
+# sudo systemctl start libvirtd
+# sudo sed --in-place "s/#user = \"libvirt-qemu\"/user = \"$username\"/g" /etc/libvirt/qemu.conf
+# sudo sed --in-place "s/#group = \"libvirt-qemu\"/group = \"$username\"/g" /etc/libvirt/qemu.conf
+# sudo systemctl restart libvirtd
+# sudo virsh net-autostart default
