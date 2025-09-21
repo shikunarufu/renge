3cb9e05e831d7e702e484153f3a0232e584064dd
shiku <134840576+shikunarufu@users.noreply.github.com>
2025-09-21 12:21:10 +0800

    Update vm.sh

--- main/vm.sh
+++ main/vm.sh
@@ -17,30 +17,40 @@
 #######################################
 
 # Verify the boot mode
-# bootmode="$(cat /sys/firmware/efi/fw_platform_size)"
-# if [[ "${bootmode}" == "64" ]]; then
-#   echo "System is booted in UEFI mode and has a 64-bit x64 UEFI"
-# elif [[ "${bootmode}" == "32" ]]; then
-#   echo "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
-# else
-#   echo "System may be booted in BIOS (or CSM) mode"
-#   echo "Refer to your motherboard's manual"
-#   exit
-# fi
+bootmode="$(cat /sys/firmware/efi/fw_platform_size)"
+if [[ "${bootmode}" == "64" ]]; then
+  echo "System is booted in UEFI mode and has a 64-bit x64 UEFI"
+elif [[ "${bootmode}" == "32" ]]; then
+  echo "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
+else
+  echo "System may be booted in BIOS (or CSM) mode"
+  echo "Refer to your motherboard's manual"
+  exit
+fi
 
 # Verify SVM mode
-# grep --extended-regexp --only-matching 'svm' /proc/cpuinfo
+grep --extended-regexp --only-matching 'svm' /proc/cpuinfo
 
 # Verify NX mode
-# sudo dmesg | grep 'Execute Disable'
+sudo dmesg | grep 'Execute Disable'
 
 # Verify IOMMU
-# ls /sys/class/iommu/
+ls /sys/class/iommu/
 
-# Virtual Machine
-# sudo sed --in-place "s/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet video=efifb:off\"/g" /etc/default/grub
-# sudo grub-mkconfig -o /boot/grub/grub.cfg
-# reboot
+# Editing GRUB with ACS override patch
+sudo sed --in-place "s/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet video=efifb:off pcie_acs_override=downstream,multifunction\"/g" /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+reboot
+
+# IOMMU Groups
+shopt -s nullglob
+for g in /sys/kernel/iommu_groups/*; do
+  echo "IOMMU Group ${g##*/}:"
+  for d in $g/devices/*; do
+    echo -e "\t$(lspci -nns ${d##*/})"
+  done;
+done;
+
 # yes y | sudo pacman -S virt-manager qemu-full vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf
 # sudo sed --in-place 's/#unix_sock_group = \"libvirt\"/unix_sock_group = \"libvirt\"/g' /etc/libvirt/libvirtd.conf
 # sudo sed --in-place 's/#unix_sock_rw_perms = \"0770\"/unix_sock_rw_perms = \"0770\"/g' /etc/libvirt/libvirtd.conf
