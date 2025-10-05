b62def39bed2920c569a04798928f53146f76633
shiku <134840576+shikunarufu@users.noreply.github.com>
2025-10-05 19:01:18 +0800

    Update install.sh

--- main/install.sh
+++ main/install.sh
@@ -42,37 +42,36 @@ user_passwd="narufu"
 clear
 
 # Set text colors
-# ok_text() {
-#   printf "["
-#   printf "\e[0;32m"
-#   printf "  OK  "
-#   printf "\e[0m"
-#   printf "]"
-#   printf "\e[10G"
-#   if [[ $1 == *" "* ]]; then
-#     local text_1=${1%% *}
-#     local text_2=${1#* }
-#     printf "%s \e[1;37m%s\e[0m\n" "${text_1}" "${text_2}"
-#   else
-#     printf "%s\n" "$1"
-#   fi
-# }
-
-# fail_text() {
-#   printf "["
-#   printf "\e[0;31m"
-#   printf "FAILED"
-#   printf "\e[0m"
-#   printf "]"
-#   printf "\e[10G"
-#   if [[ $1 == *" "* ]]; then
-#     local text_1=${1%% *}
-#     local text_2=${1#* }
-#     printf "%s \e[1;37m%s\e[0m\n" "${text_1}" "${text_2}"
-#   else
-#     printf "%s\n" "$1"
-#   fi
-# }
+ok_text() {
+  printf "["
+  printf "\e[0;32m"
+  printf "  OK  "
+  printf "\e[0m"
+  printf "]"
+  printf "\e[10G"
+  if [[ $1 == *" "* ]]; then
+    local text_1=${1%% *}
+    local text_2=${1#* }
+    printf "%s \e[1;37m%s\e[0m\n" "${text_1}" "${text_2}"
+  else
+    printf "%s\n" "$1"
+  fi
+}
+fail_text() {
+  printf "["
+  printf "\e[0;31m"
+  printf "FAILED"
+  printf "\e[0m"
+  printf "]"
+  printf "\e[10G"
+  if [[ $1 == *" "* ]]; then
+    local text_1=${1%% *}
+    local text_2=${1#* }
+    printf "%s \e[1;37m%s\e[0m\n" "${text_1}" "${text_2}"
+  else
+    printf "%s\n" "$1"
+  fi
+}
 
 #######################################
 # Pre-Installation
@@ -81,23 +80,19 @@ clear
 # Set the console keyboard layout and font
 loadkeys "${console_keyboard}"
 setfont "${console_font}"
-# ok_text "Set the console keyboard layout and font"
-printf "%s\n" "#######################################"
 
 # Verify the boot mode
 bootmode="$(cat /sys/firmware/efi/fw_platform_size)"
 if [[ "${bootmode}" == "64" ]]; then
-  printf "\033[9C%s\n" "System is booted in UEFI mode and has a 64-bit x64 UEFI"
+  printf "%s\n" "System is booted in UEFI mode and has a 64-bit x64 UEFI"
 elif [[ "${bootmode}" == "32" ]]; then
-  printf "\033[9C%s\n" "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
+  printf "%s\n" "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
 else
-  # fail_text "System may be booted in BIOS (or CSM) mode"
-  printf "\033[9C%s\n" "Refer to your motherboard's manual"
+  fail_text "System may be booted in BIOS (or CSM) mode"
+  fail_text "Refer to your motherboard's manual"
   rm --force --recursive renge
   exit
 fi
-# ok_text "Verified the boot mode"
-printf "%s\n" "#######################################"
 
 # Connect to the internet
 if ! ping -c 1 archlinux.org; then
@@ -105,13 +100,9 @@ if ! ping -c 1 archlinux.org; then
   rm --force --recursive renge
   exit
 fi
-# ok_text "Connected to the internet"
-printf "%s\n" "#######################################"
 
 # Update the system clock
 timedatectl set-ntp true
-# ok_text "Updated the system clock"
-printf "%s\n" "#######################################"
 
 # Partition the disks
 umount --all-targets --recursive /mnt
@@ -125,8 +116,6 @@ sgdisk --new=3:0:0 --typecode=3:8300 --change-name=3:"Linux filesystem" /dev/"${
 sgdisk --new=1:0:0 --typecode=1:8300 --change-name=1:"Linux filesystem" /dev/"${hdd}"
 partprobe /dev/"${ssd}"
 partprobe /dev/"${hdd}"
-# ok_text "Partitioned the disks"
-printf "%s\n" "#######################################"
 
 # Format the partitions
 root_partition="${ssd}3"
@@ -137,7 +126,6 @@ mkfs.ext4 -F /dev/"${root_partition}"
 mkfs.ext4 -F /dev/"${home_partition}"
 mkswap /dev/"${swap_partition}"
 mkfs.fat -F 32 /dev/"${efi_system_partition}"
-ok_text "Formatted the partitions"
 
 # Mount the file systems
 mount /dev/"${root_partition}" /mnt
@@ -146,8 +134,6 @@ mount /dev/"${efi_system_partition}" /mnt/boot
 mkdir /mnt/home
 mount /dev/"${home_partition}" /mnt/home
 swapon /dev/"${swap_partition}"
-# ok_text "Mounted the file systems"
-printf "%s\n" "#######################################"
 
 #######################################
 # Installation
@@ -159,22 +145,16 @@ thread=$(nproc)
 sed --in-place "s/ParallelDownloads = 5/ParallelDownloads = $thread/g" /etc/pacman.conf
 pacman -S --noconfirm archlinux-keyring
 reflector --save /etc/pacman.d/mirrorlist --sort rate --threads 12 --latest 200 --protocol https,http
-# ok_text "Selected the mirrors"
-printf "%s\n" "#######################################"
 
 # Parallel compilation
 core=$(grep --count ^processor /proc/cpuinfo)
 sed --in-place "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$core\"/g" /etc/makepkg.conf
-# ok_text "Configured parallel compilation"
-printf "%s\n" "#######################################"
 
 # Install essential packages
 curl --silent --location https://raw.githubusercontent.com/shikunarufu/renge/refs/heads/main/main/pkgs/install-pacstrap-pkglist.txt >> install-pacstrap-pkglist.txt
 grep --extended-regexp --only-matching '^[^(#|[:space:])]*' install-pacstrap-pkglist.txt | sort --output=install-pacstrap-pkglist.txt --unique
 pacstrap -K /mnt - < install-pacstrap-pkglist.txt
 rm install-pacstrap-pkglist.txt
-# ok_text "Installed essential packages"
-printf "%s\n" "#######################################"
 
 #######################################
 # Configure The System
@@ -182,8 +162,6 @@ printf "%s\n" "#######################################"
 
 # Fstab
 genfstab -U /mnt >> /mnt/etc/fstab
-# ok_text "Generated fstab"
-printf "%s\n" "#######################################"
 
 # Prepare for chroot
 cat << EOF > /mnt/configure.sh
@@ -194,16 +172,14 @@ sed --in-place 's/#Color/Color/g' /etc/pacman.conf
 sed --in-place 's/ParallelDownloads = 5/ParallelDownloads = 12/g' /etc/pacman.conf
 core=$(grep --count ^processor /proc/cpuinfo)
 sed --in-place "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j\$core\"/g" /etc/makepkg.conf
-printf "%s\n" "#######################################"
 
 # Time
 ln -sf /usr/share/zoneinfo/"${time_zone}" /etc/localtime
 hwclock --systohc
 if ! systemctl enable systemd-timesyncd.service; then
-  printf "%s\n" "Failed to enable systemd-timesyncd.service"
+  fail_text "Failed to enable systemd-timesyncd.service"
   exit
 fi
-printf "%s\n" "#######################################"
 
 # Localization
 sed --in-place 's/#${utf_locale}/${utf_locale}/g' /etc/locale.gen
@@ -211,61 +187,52 @@ sed --in-place 's/#${iso_locale}/${iso_locale}/g' /etc/locale.gen
 locale-gen
 printf "%s\n" "LANG="${language}"" >> /etc/locale.conf
 printf "%s\n" "KEYMAP="${console_keyboard}"" >> /etc/vconsole.conf
-printf "%s\n" "#######################################"
 
 # Network configuration
 printf "%s\n" "${hostname}" >> /etc/hostname
 if ! systemctl enable NetworkManager.service; then
-  printf "%s\n" "Failed to enable NetworkManager.service"
+  fail_text "Failed to enable NetworkManager.service"
   exit
 fi
-printf "%s\n" "#######################################"
 
 # Root password
 printf "%s\n%s" "${root_passwd}" "${root_passwd}" | passwd
-printf "%s\n" "#######################################"
 
 # Users and groups
 useradd --create-home --groups wheel "${username}"
 printf "%s\n%s" "${user_passwd}" "${user_passwd}" | passwd "${username}"
-printf "%s\n" "#######################################"
 
 # Security
 sed --in-place 's/# %wheel/%wheel/g' /etc/sudoers
 sed --in-place 's/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
 printf "%s\n" "Defaults passwd_timeout=0" >> /etc/sudoers
-printf "%s\n" "#######################################"
 
 # Repositories
 sed --in-place 's|#\[multilib\]|\[multilib\]|g' /etc/pacman.conf
 sed --in-place '93s|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|g' /etc/pacman.conf
 pacman -Syu --noconfirm
-printf "%s\n" "#######################################"
 
 # Installation
 curl --silent --location https://raw.githubusercontent.com/shikunarufu/renge/refs/heads/main/main/pkgs/install-pacman-pkglist.txt >> install-pacman-pkglist.txt
 grep --extended-regexp --only-matching '^[^(#|[:space:])]*' install-pacman-pkglist.txt | sort --output=install-pacman-pkglist.txt --unique
 pacman -S --noconfirm --needed - < install-pacman-pkglist.txt
 rm install-pacman-pkglist.txt
-printf "%s\n" "#######################################"
 
 # Boot loader
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 sed --in-place "s/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet video=efifb:off pcie_acs_override=downstream,multifunction\"/g" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-printf "%s\n" "#######################################"
 
 # System services
 if ! systemctl enable paccache.timer; then
-  printf "%s\n" "Failed to enable paccache.timer"
+  fail_text "Failed to enable paccache.timer"
   exit
 fi
 xdg-user-dirs-update
 if ! systemctl enable fstrim.timer; then
-  printf "%s\n" "Failed to enable fstrim.timer"
+  fail_text "Failed to enable fstrim.timer"
   exit
 fi
-printf "%s\n" "#######################################"
 EOF
 
 # Chroot
@@ -279,10 +246,11 @@ arch-chroot /mnt /configure.sh
 # Reboot
 rm /mnt/configure.sh
 umount -R /mnt
+ok_text "Nyanpasu~!"
 sec=15
 while [[ ${sec} -gt 0 ]]; do
   printf "\033[9C%s\r" "Restarting in $sec seconds"
   sleep 1
   ((sec--))
 done
-# reboot
+reboot
