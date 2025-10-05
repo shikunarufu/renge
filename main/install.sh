e9f7e39d147e4cabe0deeb2840a4fd33547565e3
shiku <134840576+shikunarufu@users.noreply.github.com>
2025-10-05 15:21:24 +0800

    Update install.sh

--- main/install.sh
+++ main/install.sh
@@ -41,27 +41,6 @@ user_passwd="narufu"
 # Clear the terminal screen
 clear
 
-# Connect to the internet
-if ! ping -c 1 archlinux.org; then
-  printf "%s\n" "Failed to connect to the internet"
-  rm --force --recursive renge
-  exit
-fi
-
-# Add developer keys
-if ! pacman -S --noconfirm archlinux-keyring; then
-  printf "%s\n" "Failed to add developer keys"
-  rm --force --recursive renge
-  exit
-fi
-
-# Install gum
-if ! pacman -S --noconfirm --needed gum; then
-  printf "%s\n" "Failed to install gum"
-  rm --force --recursive renge
-  exit
-fi
-
 # Set text colors
 ok_text() {
   printf "["
@@ -79,102 +58,73 @@ ok_text() {
   fi
 }
 
-# Clear the terminal screen
-clear
-
 #######################################
 # Pre-Installation
 #######################################
 
 # Set the console keyboard layout and font
-keyboard_font() {
-  loadkeys "${console_keyboard}"
-  setfont "${console_font}"
-}
-export -f keyboard_font
-gum spin --spinner "line" --title "Setting the console keyboard layout and font" -- bash -c keyboard_font
+loadkeys "${console_keyboard}" | pv --name
+setfont "${console_font}" | pv
 ok_text "Set the console keyboard layout and font"
 
-line, pulse, points
-
 # Verify the boot mode
-boot() {
-  bootmode="$(cat /sys/firmware/efi/fw_platform_size)"
-  if [[ "${bootmode}" == "64" ]]; then
-    printf "%s\n" "System is booted in UEFI mode and has a 64-bit x64 UEFI"
-  elif [[ "${bootmode}" == "32" ]]; then
-    printf "%s\n" "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
-  else
-    printf "%s\n" "System may be booted in BIOS (or CSM) mode"
-    printf "%s\n" "Refer to your motherboard's manual"
-    rm --force --recursive renge
-    exit
-  fi
-}
-export -f boot
-gum spin --spinner "line" --title "Verifying the boot mode" -- bash -c boot
+bootmode="$(cat /sys/firmware/efi/fw_platform_size)"
+if [[ "${bootmode}" == "64" ]]; then
+  printf "%s\n" "System is booted in UEFI mode and has a 64-bit x64 UEFI"
+elif [[ "${bootmode}" == "32" ]]; then
+  printf "%s\n" "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
+else
+  printf "%s\n" "System may be booted in BIOS (or CSM) mode"
+  printf "%s\n" "Refer to your motherboard's manual"
+  rm --force --recursive renge
+  exit
+fi
 ok_text "Verified the boot mode"
 
 # Connect to the internet
-internet() {
-  if ! ping -c 1 archlinux.org; then
-    printf "%s\n" "Failed to connect to the internet"
-    rm --force --recursive renge
-    exit
-  fi
-}
-export -f internet
-gum spin --spinner "line" --title "Connecting to the internet" -- bash -c internet
+if ! ping -c 1 archlinux.org | pv; then
+  printf "%s\n" "Failed to connect to the internet"
+  rm --force --recursive renge
+  exit
+fi
 ok_text "Connected to the internet"
 
 # Update the system clock
-gum spin --spinner "line" --title "Updating the system clock" -- timedatectl set-ntp true
+timedatectl set-ntp true | pv
 ok_text "Updated the system clock"
 
 # Partition the disks
-partition() {
-  umount --all-targets --recursive /mnt
-  sgdisk --zap-all /dev/"${ssd}"
-  sgdisk --zap-all /dev/"${hdd}"
-  sgdisk --set-alignment=2048 --clear /dev/"${ssd}"
-  sgdisk --set-alignment=2048 --clear /dev/"${hdd}"
-  sgdisk --new=1:0:+1G --typecode=1:EF00 --change-name=1:"EFI system partition" /dev/"${ssd}"
-  sgdisk --new=2:0:+4G --typecode=2:8200 --change-name=2:"Linux swap" /dev/"${ssd}"
-  sgdisk --new=3:0:0 --typecode=3:8300 --change-name=3:"Linux filesystem" /dev/"${ssd}"
-  sgdisk --new=1:0:0 --typecode=1:8300 --change-name=1:"Linux filesystem" /dev/"${hdd}"
-  partprobe /dev/"${ssd}"
-  partprobe /dev/"${hdd}"
-}
-export -f partition
-gum spin --spinner "line" --title "Partitioning the disks" -- bash -c partition
+umount --all-targets --recursive /mnt | pv
+sgdisk --zap-all /dev/"${ssd}" | pv
+sgdisk --zap-all /dev/"${hdd}" | pv
+sgdisk --set-alignment=2048 --clear /dev/"${ssd}" | pv
+sgdisk --set-alignment=2048 --clear /dev/"${hdd}" | pv
+sgdisk --new=1:0:+1G --typecode=1:EF00 --change-name=1:"EFI system partition" /dev/"${ssd}" | pv
+sgdisk --new=2:0:+4G --typecode=2:8200 --change-name=2:"Linux swap" /dev/"${ssd}" | pv
+sgdisk --new=3:0:0 --typecode=3:8300 --change-name=3:"Linux filesystem" /dev/"${ssd}" | pv
+sgdisk --new=1:0:0 --typecode=1:8300 --change-name=1:"Linux filesystem" /dev/"${hdd}" | pv
+partprobe /dev/"${ssd}" | pv
+partprobe /dev/"${hdd}" | pv
 ok_text "Partitioned the disks"
 
 # Format the partitions
-format() {
-  root_partition="${ssd}3"
-  home_partition="${hdd}1"
-  swap_partition="${ssd}2"
-  efi_system_partition="${ssd}1"
-  mkfs.ext4 -F /dev/"${root_partition}"
-  mkfs.ext4 -F /dev/"${home_partition}"
-  mkswap /dev/"${swap_partition}"
-  mkfs.fat -F 32 /dev/"${efi_system_partition}"
-}
-export -f format
-gum spin --spinner "line" --title "Formatting the partitions" --spinner.foreground "#0000FF" -- bash -c format
+root_partition="${ssd}3" | pv
+home_partition="${hdd}1" | pv
+swap_partition="${ssd}2" | pv
+efi_system_partition="${ssd}1" | pv
+mkfs.ext4 -F /dev/"${root_partition}" | pv
+mkfs.ext4 -F /dev/"${home_partition}" | pv
+mkswap /dev/"${swap_partition}" | pv
+mkfs.fat -F 32 /dev/"${efi_system_partition}" | pv
 ok_text "Formatted the partitions"
 
 # Mount the file systems
-mount_files() {
-  mount /dev/"${root_partition}" /mnt
-  mkdir /mnt/boot
-  mount /dev/"${efi_system_partition}" /mnt/boot
-  mkdir /mnt/home
-  mount /dev/"${home_partition}" /mnt/home
-  swapon /dev/"${swap_partition}"
-}
-export -f mount_files
-gum spin --spinner "line" --title "Mounting the file systems" --spinner.foreground "#0000FF" -- bash -c mount_files
+mount /dev/"${root_partition}" /mnt | pv
+mkdir /mnt/boot | pv
+mount /dev/"${efi_system_partition}" /mnt/boot | pv
+mkdir /mnt/home | pv
+mount /dev/"${home_partition}" /mnt/home | pv
+swapon /dev/"${swap_partition}" | pv
 ok_text "Mounted the file systems"
 
 #######################################
@@ -182,35 +132,24 @@ ok_text "Mounted the file systems"
 #######################################
 
 # Select the mirrors
-mirrors() {
-  sed --in-place 's/#Color/Color/g' /etc/pacman.conf
-  thread=$(nproc)
-  sed --in-place "s/ParallelDownloads = 5/ParallelDownloads = $thread/g" /etc/pacman.conf
-  # pacman -S --noconfirm archlinux-keyring
-  reflector --save /etc/pacman.d/mirrorlist --sort rate --threads 12 --latest 200 --protocol https,http
-}
-export -f mirrors
-gum spin --spinner "line" --title "Selecting the mirrors" --spinner.foreground "#0000FF" -- bash -c mirrors
+sed --in-place 's/#Color/Color/g' /etc/pacman.conf | pv
+thread=$(nproc)
+sed --in-place "s/ParallelDownloads = 5/ParallelDownloads = $thread/g" /etc/pacman.conf | pv
+pacman -S --noconfirm archlinux-keyring | pv
+reflector --save /etc/pacman.d/mirrorlist --sort rate --threads 12 --latest 200 --protocol https,http | pv
 ok_text "Selected the mirrors"
 
 # Parallel compilation
-parallel() {
-  core=$(grep --count ^processor /proc/cpuinfo)
-  sed --in-place "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$core\"/g" /etc/makepkg.conf
-}
-export -f parallel
-gum spin --spinner "line" --title "Configuring parallel compilation" --spinner.foreground "#0000FF" -- bash -c parallel
+core=$(grep --count ^processor /proc/cpuinfo)
+sed --in-place "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$core\"/g" /etc/makepkg.conf | pv
 ok_text "Configured parallel compilation"
 
 # Install essential packages
-pacstrap_pkgs() {
-  curl --silent --location https://raw.githubusercontent.com/shikunarufu/renge/refs/heads/main/main/pkgs/install-pacstrap-pkglist.txt >> install-pacstrap-pkglist.txt
-  grep --extended-regexp --only-matching '^[^(#|[:space:])]*' install-pacstrap-pkglist.txt | sort --output=install-pacstrap-pkglist.txt --unique
-  pacstrap -K /mnt - < install-pacstrap-pkglist.txt
-  rm install-pacstrap-pkglist.txt
-}
-export -f pacstrap_pkgs
-gum spin --spinner "line" --title "Installing essential packages" --spinner.foreground "#0000FF" -- bash -c pacstrap_pkgs
+curl --silent --location https://raw.githubusercontent.com/shikunarufu/renge/refs/heads/main/main/pkgs/install-pacstrap-pkglist.txt >> install-pacstrap-pkglist.txt | pv
+grep --extended-regexp --only-matching '^[^(#|[:space:])]*' install-pacstrap-pkglist.txt | sort --output=install-pacstrap-pkglist.txt --unique | pv
+pacstrap -K /mnt - < install-pacstrap-pkglist.txt | pv
+rm install-pacstrap-pkglist.txt
+
 ok_text "Installed essential packages"
 
 #######################################
@@ -218,7 +157,7 @@ ok_text "Installed essential packages"
 #######################################
 
 # Fstab
-gum spin --spinner "line" --title "Generating fstab" --spinner.foreground "#0000FF" -- genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 ok_text "Generated fstab"
 
 # Prepare for chroot
@@ -304,10 +243,10 @@ arch-chroot /mnt /configure.sh
 # Reboot
 rm /mnt/configure.sh
 umount -R /mnt
-sec=10
+sec=15
 while [[ ${sec} -gt 0 ]]; do
   printf "Restarting in $sec seconds \r"
   sleep 1
   ((sec--))
 done
-reboot
+# reboot
