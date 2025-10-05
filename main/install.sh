b0d390daac815850e929a4ad7493107503f3afbf
shiku <134840576+shikunarufu@users.noreply.github.com>
2025-10-05 16:40:02 +0800

    Update install.sh

--- main/install.sh
+++ main/install.sh
@@ -58,73 +58,89 @@ ok_text() {
   fi
 }
 
+fail_text() {
+  printf "["
+  printf "\e[0;31m"
+  printf "FAILED"
+  printf "\e[0m"
+  printf "]"
+  printf "\e[10G"
+  if [[ $1 == *" "* ]]; then
+    local text_1=${1%% *}
+    local text_2=${1#* }
+    printf "%s \e[1;37m%s\e[0m\n" "${text_1}" "${text_2}"
+  else
+    printf "%s\n" "$1"
+  fi
+}
+
 #######################################
 # Pre-Installation
 #######################################
 
 # Set the console keyboard layout and font
-loadkeys "${console_keyboard}" | pv --name
-setfont "${console_font}" | pv
+loadkeys "${console_keyboard}"
+setfont "${console_font}"
 ok_text "Set the console keyboard layout and font"
 
 # Verify the boot mode
 bootmode="$(cat /sys/firmware/efi/fw_platform_size)"
 if [[ "${bootmode}" == "64" ]]; then
-  printf "%s\n" "System is booted in UEFI mode and has a 64-bit x64 UEFI"
+  printf "\033[9C%s\n" "System is booted in UEFI mode and has a 64-bit x64 UEFI"
 elif [[ "${bootmode}" == "32" ]]; then
-  printf "%s\n" "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
+  printf "\033[9C%s\n" "System is booted in UEFI mode and has a 32-bit IA32 UEFI"
 else
-  printf "%s\n" "System may be booted in BIOS (or CSM) mode"
-  printf "%s\n" "Refer to your motherboard's manual"
+  fail_text "System may be booted in BIOS (or CSM) mode"
+  printf "\033[9C%s\n" "Refer to your motherboard's manual"
   rm --force --recursive renge
   exit
 fi
 ok_text "Verified the boot mode"
 
 # Connect to the internet
-if ! ping -c 1 archlinux.org | pv; then
-  printf "%s\n" "Failed to connect to the internet"
+if ! ping -c 1 archlinux.org; then
+  fail_text "Failed to connect to the internet"
   rm --force --recursive renge
   exit
 fi
 ok_text "Connected to the internet"
 
 # Update the system clock
-timedatectl set-ntp true | pv
+timedatectl set-ntp true
 ok_text "Updated the system clock"
 
 # Partition the disks
-umount --all-targets --recursive /mnt | pv
-sgdisk --zap-all /dev/"${ssd}" | pv
-sgdisk --zap-all /dev/"${hdd}" | pv
-sgdisk --set-alignment=2048 --clear /dev/"${ssd}" | pv
-sgdisk --set-alignment=2048 --clear /dev/"${hdd}" | pv
-sgdisk --new=1:0:+1G --typecode=1:EF00 --change-name=1:"EFI system partition" /dev/"${ssd}" | pv
-sgdisk --new=2:0:+4G --typecode=2:8200 --change-name=2:"Linux swap" /dev/"${ssd}" | pv
-sgdisk --new=3:0:0 --typecode=3:8300 --change-name=3:"Linux filesystem" /dev/"${ssd}" | pv
-sgdisk --new=1:0:0 --typecode=1:8300 --change-name=1:"Linux filesystem" /dev/"${hdd}" | pv
-partprobe /dev/"${ssd}" | pv
-partprobe /dev/"${hdd}" | pv
+umount --all-targets --recursive /mnt
+sgdisk --zap-all /dev/"${ssd}"
+sgdisk --zap-all /dev/"${hdd}"
+sgdisk --set-alignment=2048 --clear /dev/"${ssd}"
+sgdisk --set-alignment=2048 --clear /dev/"${hdd}"
+sgdisk --new=1:0:+1G --typecode=1:EF00 --change-name=1:"EFI system partition" /dev/"${ssd}"
+sgdisk --new=2:0:+4G --typecode=2:8200 --change-name=2:"Linux swap" /dev/"${ssd}"
+sgdisk --new=3:0:0 --typecode=3:8300 --change-name=3:"Linux filesystem" /dev/"${ssd}"
+sgdisk --new=1:0:0 --typecode=1:8300 --change-name=1:"Linux filesystem" /dev/"${hdd}"
+partprobe /dev/"${ssd}"
+partprobe /dev/"${hdd}"
 ok_text "Partitioned the disks"
 
 # Format the partitions
-root_partition="${ssd}3" | pv
-home_partition="${hdd}1" | pv
-swap_partition="${ssd}2" | pv
-efi_system_partition="${ssd}1" | pv
-mkfs.ext4 -F /dev/"${root_partition}" | pv
-mkfs.ext4 -F /dev/"${home_partition}" | pv
-mkswap /dev/"${swap_partition}" | pv
-mkfs.fat -F 32 /dev/"${efi_system_partition}" | pv
+root_partition="${ssd}3"
+home_partition="${hdd}1"
+swap_partition="${ssd}2"
+efi_system_partition="${ssd}1"
+mkfs.ext4 -F /dev/"${root_partition}"
+mkfs.ext4 -F /dev/"${home_partition}"
+mkswap /dev/"${swap_partition}"
+mkfs.fat -F 32 /dev/"${efi_system_partition}"
 ok_text "Formatted the partitions"
 
 # Mount the file systems
-mount /dev/"${root_partition}" /mnt | pv
-mkdir /mnt/boot | pv
-mount /dev/"${efi_system_partition}" /mnt/boot | pv
-mkdir /mnt/home | pv
-mount /dev/"${home_partition}" /mnt/home | pv
-swapon /dev/"${swap_partition}" | pv
+mount /dev/"${root_partition}" /mnt
+mkdir /mnt/boot
+mount /dev/"${efi_system_partition}" /mnt/boot
+mkdir /mnt/home
+mount /dev/"${home_partition}" /mnt/home
+swapon /dev/"${swap_partition}"
 ok_text "Mounted the file systems"
 
 #######################################
@@ -132,24 +148,23 @@ ok_text "Mounted the file systems"
 #######################################
 
 # Select the mirrors
-sed --in-place 's/#Color/Color/g' /etc/pacman.conf | pv
+sed --in-place 's/#Color/Color/g' /etc/pacman.conf
 thread=$(nproc)
-sed --in-place "s/ParallelDownloads = 5/ParallelDownloads = $thread/g" /etc/pacman.conf | pv
-pacman -S --noconfirm archlinux-keyring | pv
-reflector --save /etc/pacman.d/mirrorlist --sort rate --threads 12 --latest 200 --protocol https,http | pv
+sed --in-place "s/ParallelDownloads = 5/ParallelDownloads = $thread/g" /etc/pacman.conf
+pacman -S --noconfirm archlinux-keyring
+reflector --save /etc/pacman.d/mirrorlist --sort rate --threads 12 --latest 200 --protocol https,http
 ok_text "Selected the mirrors"
 
 # Parallel compilation
 core=$(grep --count ^processor /proc/cpuinfo)
-sed --in-place "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$core\"/g" /etc/makepkg.conf | pv
+sed --in-place "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$core\"/g" /etc/makepkg.conf
 ok_text "Configured parallel compilation"
 
 # Install essential packages
-curl --silent --location https://raw.githubusercontent.com/shikunarufu/renge/refs/heads/main/main/pkgs/install-pacstrap-pkglist.txt >> install-pacstrap-pkglist.txt | pv
-grep --extended-regexp --only-matching '^[^(#|[:space:])]*' install-pacstrap-pkglist.txt | sort --output=install-pacstrap-pkglist.txt --unique | pv
-pacstrap -K /mnt - < install-pacstrap-pkglist.txt | pv
+curl --silent --location https://raw.githubusercontent.com/shikunarufu/renge/refs/heads/main/main/pkgs/install-pacstrap-pkglist.txt >> install-pacstrap-pkglist.txt
+grep --extended-regexp --only-matching '^[^(#|[:space:])]*' install-pacstrap-pkglist.txt | sort --output=install-pacstrap-pkglist.txt --unique
+pacstrap -K /mnt - < install-pacstrap-pkglist.txt
 rm install-pacstrap-pkglist.txt
-
 ok_text "Installed essential packages"
 
 #######################################
