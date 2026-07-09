d97a9b7b642759130f14650e63376504b4ad002e
shiku <134840576+shikunarufu@users.noreply.github.com>
2026-07-09 21:57:31 +0800

    Enhance install.sh for Arch Linux installation
    Added steps to set keyboard layout, verify boot mode, check internet connection, partition disks, format partitions, and mount file systems.

--- install.sh
+++ install.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+# Set the console keyboard layout and font
+loadkeys us
+setfont Lat2-Terminus16
+
+# Verify the boot mode
+cat /sys/firmware/efi/fw_platform_size
+
+# Verify the internet connection
+ping ping.archlinux.org
+
+# Update the system clock
+timedatectl
+
+# Partition the disks
+wipefs --all /dev/vda
+dd if=/dev/zero of=/dev/vda bs=1M count=100 status=progress
+parted --script /dev/vda mklabel gpt
+parted --script --align optimal /dev/vda mkpart "" fat32 0% 4096Mib
+parted --script --align optimal /dev/vda mkpart "" btrfs 4096Mib 100%
+wipefs --all /dev/vdb
+dd if=/dev/zero of=/dev/vdb bs=1M count=100 status=progress
+parted --script /dev/vdb mklabel gpt
+parted --script --align optimal /dev/vdb mkpart "" btrfs 0% 100%
+wipefs --all /dev/vdc
+dd if=/dev/zero of=/dev/vdc bs=1M count=100 status=progress
+parted --script /dev/vdc mklabel gpt
+parted --script --align optimal /dev/vdc mkpart "" btrfs 0% 100%
+
+# Format the partitions
+mkfs.fat -F 32 /dev/vda1
+mkfs.btrfs /dev/vda2
+mkfs.btrfs /dev/vdb1
+mkfs.btrfs /dev/vdc1
+
+# Mount the file systems
+mount /dev/vda2 /mnt
+mount --mkdir /dev/vda1 /mnt/boot
+mount --mkdir /dev/vdb1 /mnt/home
+
 # Shiku's Arch Linux Installation Script
 
 # This script automates the Arch Linux installation process
